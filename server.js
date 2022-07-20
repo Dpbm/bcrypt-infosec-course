08150cb5b1d16dbfb16ede1bfc3d185ec23a94d6
Dpbm <dpbm136@gmail.com>
2022-07-20 09:13:10 -0300

    changed return variables names

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-bcrypt.hash(myPlaintextPassword, saltRounds, (error, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
 
-    bcrypt.compare(myPlaintextPassword, hash, (error, response) => {
-        console.log(response);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res);
     });
 });
 
