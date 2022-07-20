eec4baa6f2ed617df2f2634d2abd715f32b554ab
Dpbm <dpbm136@gmail.com>
2022-07-20 09:21:52 -0300

    added sync functions to the right place

--- server.js
+++ server.js
@@ -22,15 +22,14 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 
 //END_ASYNC
 
-var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
-console.log(hash);
-var result = bcrypt.compareSync(myPlaintextPassword, hash);
-console.log(result);
-
 
 //START_SYNC
 
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
 
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
